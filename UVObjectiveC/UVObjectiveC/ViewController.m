7d567716257002e72d6d3e6ac1dc1ef57a908915
Unknown <matheuspgarcia@gmail.com>
2018-07-25 15:18:33 -0300

    Added api request

--- UVObjectiveC/UVObjectiveC/ViewController.m
+++ UVObjectiveC/UVObjectiveC/ViewController.m
@@ -44,7 +44,28 @@
 
 - (void)updateInfo:(CLLocation *)location {
     NSString *locationText = [NSString stringWithFormat:@"lat: %f, lon: %f", location.coordinate.latitude, location.coordinate.longitude];
+    [self request:location];
     locationLabel.text = locationText;
+}
+
+- (void)request:(CLLocation *)location {
+
+    NSString *urlString = [NSString stringWithFormat:@"https://api.openuv.io/api/v1/uv?lat=%f&lng=%f", location.coordinate.latitude, location.coordinate.longitude];
+    NSString *key = @"c3b27e3623430b041a771d2b9e6af652";
+    NSString *headerField = @"x-access-token";
+
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:urlString]];
+    [request setHTTPMethod:@"GET"];
+    [request addValue:key forHTTPHeaderField:headerField];
+
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    [[session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSString *requestReply = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+        NSData * responseData = [requestReply dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+        NSLog(@"requestReply: %@", jsonDict);
+    }] resume];
 }
 
 - (IBAction)updateWasPressed:(id)sender {
